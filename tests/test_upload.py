a9db55f7c30ce260df75843e3d080f18dde09ab9
liu xue yan <liu_xue_yan@foxmail.com>
2019-06-14 22:31:23 +0800

    fix: Windows encoding errors in test-cases

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -18,13 +18,14 @@ KS3_BUCKET = os.environ['KS3_BUCKET']
 
 class SmallTextFileUploadTestCase(unittest.TestCase):
     KEY = 'README.md'
+    ENCODING = 'utf-8'
 
     def setUp(self):
         self.client = Client(endpoint=KS3_ENDPOINT)
         self.upload = partial(self.client.send, method='put', bucket_name=KS3_BUCKET, object_key=self.KEY)
 
     def test_open(self):
-        with open(self.KEY) as fp:
+        with open(self.KEY, encoding=self.ENCODING) as fp:
             self.upload(data=fp)
 
     def test_open_binary(self):
@@ -36,7 +37,7 @@ class SmallTextFileUploadTestCase(unittest.TestCase):
         self.upload(data=data)
 
     def test_string(self):
-        with open(self.KEY) as fp:
+        with open(self.KEY, encoding=self.ENCODING) as fp:
             data = fp.read()
         self.upload(data=data)
 
@@ -46,7 +47,7 @@ class SmallTextFileUploadTestCase(unittest.TestCase):
         self.upload(data=data)
 
     def test_string_io(self):
-        with open(self.KEY) as fp:
+        with open(self.KEY, encoding=self.ENCODING) as fp:
             data = io.StringIO(fp.read())
         self.upload(data=data)
 
@@ -56,7 +57,7 @@ class SmallTextFileUploadTestCase(unittest.TestCase):
         self.upload(data=data)
 
     def test_wrong_checksum(self):
-        with open(self.KEY) as fp:
+        with open(self.KEY, encoding=self.ENCODING) as fp:
             with self.assertRaises(Ks3Error) as context:
                 self.upload(data=fp, content_md5='fake')
         self.assertEqual('The Content-MD5 you specified is not valid.', context.exception.message)
